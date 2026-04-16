8ac44f363614769f42df794b9c681a1289ab7cf0
bjoern555 <bjoern-wagner@outlook.com>
2026-04-16 16:41:02 +0200

    Knapsack implemented and Benchmark

--- Algorithms.hs
+++ Algorithms.hs
@@ -1,4 +1,4 @@
-module Algorithms (fibSlow, fibFast, levSlow, levFast) where
+module Algorithms (fibSlow, fibFast, levSlow, levFast, knapSlow, knapFast) where
 
 import Data.Function (fix)
 import Memo (memoFix, memoFix2)
@@ -30,3 +30,19 @@ levSlow s1 s2 = fix (levenshteinF s1 s2) (length s1) (length s2)
 
 levFast :: String -> String -> Int
 levFast s1 s2 = memoFix2 (levenshteinF s1 s2) (length s1) (length s2)
+
+type Item = (Int, Int)
+
+knapsackF :: [Item] -> (Int -> Int -> Int) -> Int -> Int -> Int
+knapsackF items rec i w
+  | i == 0 || w == 0 = 0
+  | weight > w       = rec (i - 1) w
+  | otherwise        = max (rec (i - 1) w) (val + rec (i - 1) (w - weight))
+  where
+    (val, weight) = items !! (i - 1)
+
+knapSlow :: [Item] -> Int -> Int
+knapSlow items cap = fix (knapsackF items) (length items) cap
+
+knapFast :: [Item] -> Int -> Int
+knapFast items cap = memoFix2 (knapsackF items) (length items) cap
--- Main.hs
+++ Main.hs
@@ -1,20 +1,22 @@
 module Main where
 
 import Criterion.Main
-import Algorithms (fibSlow, fibFast, levSlow, levFast)
+import Algorithms (fibSlow, fibFast, levSlow, levFast, knapSlow, knapFast)
 
 main :: IO ()
 main = do
-  let word1 = "Informatiker"
-  let word2 = "Mathematiker"
+  let items = [(10,2), (20,3), (30,5), (40,7), (50,9), (15,2), (25,4), (35,6), (45,8), (55,10),
+               (12,2), (22,3), (32,5), (42,7), (52,9), (17,2), (27,4), (37,6), (47,8), (57,10)]
+  let cap = 50
 
   defaultMain 
     [ bgroup "1D: Fibonacci (n=30)" 
-        [ bench "fibSlow (Standard)" $ whnf fibSlow 30
-        , bench "fibFast (Memoized)" $ whnf fibFast 30
-        ]
-    , bgroup "2D: Levenshtein (12 letters)" 
-        [ bench "levSlow (Standard)" $ whnf (levSlow word1) word2
-        , bench "levFast (Memoized)" $ whnf (levFast word1) word2
-        ]
+        [ bench "fibSlow" $ whnf fibSlow 30
+        , bench "fibFast" $ whnf fibFast 30 ]
+    , bgroup "2D: Levenshtein" 
+        [ bench "levSlow" $ whnf (levSlow "Informatiker") "Mathematiker"
+        , bench "levFast" $ whnf (levFast "Informatiker") "Mathematiker" ]
+    , bgroup "2D: Knapsack (20 Items)" 
+        [ bench "knapSlow" $ whnf (knapSlow items) cap
+        , bench "knapFast" $ whnf (knapFast items) cap ]
     ]
