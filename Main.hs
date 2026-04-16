c7d1b177744a064541eea6c5a34eca188cf81938
bjoern555 <bjoern-wagner@outlook.com>
2026-04-16 16:29:29 +0200

    Typo

--- Main.hs
+++ Main.hs
@@ -5,16 +5,16 @@ import Algorithms (fibSlow, fibFast, levSlow, levFast)
 
 main :: IO ()
 main = do
-  let wort1 = "Informatiker"
-  let wort2 = "Mathematiker"
+  let word1 = "Informatiker"
+  let word2 = "Mathematiker"
 
   defaultMain 
     [ bgroup "1D: Fibonacci (n=30)" 
         [ bench "fibSlow (Standard)" $ whnf fibSlow 30
         , bench "fibFast (Memoized)" $ whnf fibFast 30
         ]
-    , bgroup "2D: Levenshtein (12 Buchstaben)" 
-        [ bench "levSlow (Standard)" $ whnf (levSlow wort1) wort2
-        , bench "levFast (Memoized)" $ whnf (levFast wort1) wort2
+    , bgroup "2D: Levenshtein (12 letters)" 
+        [ bench "levSlow (Standard)" $ whnf (levSlow word1) word2
+        , bench "levFast (Memoized)" $ whnf (levFast word1) word2
         ]
     ]
